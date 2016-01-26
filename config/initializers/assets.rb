28699bb57432c30e8a64ee10fa7906103b40ebdc
anamariasosam <anamariasosam@gmail.com>
2016-01-25 22:03:56 -0500

    assets settings

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,11 +4,9 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( lightbox/* )
 Rails.application.config.assets.precompile += %w( flipBook/* )
-Rails.application.config.assets.precompile += %w( fullpagejs/jquery.fullpage.min.js )
 Rails.application.config.assets.precompile += %w( footable/footable.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
