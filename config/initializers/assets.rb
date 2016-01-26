5b5123cfdba12a087ab556e2be2987e5b52a2b1a
anamariasosam <anamariasosam@gmail.com>
2016-01-26 14:46:11 -0500

    add ckeditor in assets file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( lightbox/* )
 Rails.application.config.assets.precompile += %w( flipBook/* )
 Rails.application.config.assets.precompile += %w( footable/footable.js )
+Rails.application.config.assets.precompile += %w( ckeditor/* )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 # Precompile additional assets.
