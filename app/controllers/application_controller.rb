28dd7735d91e7a1cc59e4220f8f04029a75f5cd3
anamariasosam <anamariasosam@gmail.com>
2016-02-04 22:05:41 -0500

    application devise sign up path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-    protect_from_forgery with: :exception
     def after_sign_in_path_for(resource)
+      users_dashboard_path
+    end
+
+    def after_sign_up_path_for(resource)
       sessions_connect_path
     end
 
