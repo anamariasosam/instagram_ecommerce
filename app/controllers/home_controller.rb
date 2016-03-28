e21ed93784f1c8694e4f5878eebbc6d4be2de220
anamariasosam <anamariasosam@gmail.com>
2016-03-27 22:34:20 -0500

    redirect to product code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @users = User.limit(6)
     @products =  Product.limit(5)
+
+    redirect_to "/#{@par}" if @par = params[:search]
   end
 end
