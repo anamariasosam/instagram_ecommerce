b1fddc929ea9f06030de9e8c85bbe426c9cde172
anamariasosam <anamariasosam@gmail.com>
2016-04-27 20:19:34 -0500

    validates instagram account exists

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,14 @@ class SessionsController < ApplicationController
     response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
     session[:access_token] = response.access_token
 
-    redirect_to "/users/sign_up"
+    client = Instagram.client(:access_token => session[:access_token])
+
+    if  User.exists?(:instagram_id => client.user.id)
+      flash[:notice] = "@#{client.user.username } Ya tienes una cuenta con nosotros. Inicia sesión con tus datos"
+      redirect_to "/users/sign_in"
+    else
+      redirect_to "/users/sign_up"
+    end
   end
 
 end
