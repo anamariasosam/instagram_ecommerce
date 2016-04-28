a9736e09ae1c524040801258d6ee49b09f681d2e
anamariasosam <anamariasosam@gmail.com>
2016-04-27 23:05:33 -0500

    add order to product by owner

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,7 +1,7 @@
 class CatalogController < ApplicationController
   def view
-    @products = Product.all
+    @products = Product.all.order('store_owner asc')
     @cover_products = Product.limit(6)
-    @catalog_products = Product.page(params[:page]).per(6)
+    @catalog_products = Product.page(params[:page]).per(6).order('store_owner asc')
   end
 end
