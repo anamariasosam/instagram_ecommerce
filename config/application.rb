f292a5b63621516aaed85daa05e47b3408a5722a
anamariasosa <anamariasosam@gmail.com>
2016-06-01 15:16:52 -0500

    add sendgrid

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module Catlog
     config.assets.compile = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.action_mailer.default_url_options = { host: 'catlog.herokuapp.com' }
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,3 @@
-Rails.application.routes.default_url_options[:host] = 'catlog.herokuapp.com'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -65,7 +64,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'catlog.herokuapp.com' }
-  # config.action_mailer.default_url_options = { :host => 'catlog.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'ENV["USERNAME"]',
+    :password => 'ENV["PASSWORD"]',
+    :domain => 'ENV["DOMAIN"]',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -79,19 +86,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
 end
