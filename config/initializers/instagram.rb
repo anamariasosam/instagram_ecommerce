ad8e8585a25622171bed6234e98991128e2c1479
anamariasosa <anamariasosam@gmail.com>
2016-06-01 20:41:33 -0500

    update secret

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,6 @@
 Instagram.configure do |config|
   config.client_id = "9b7a7c0bb3c24fb6b7ae1cc5b77603c8"
-  config.client_secret = "58700887349e44c7b809dc4c61e225c3"
+  config.client_secret = "784947d737dd46f580b804116a2504fa"
   # For secured endpoints only
   #config.client_ips = '<Comma separated list of IPs>'
 end
