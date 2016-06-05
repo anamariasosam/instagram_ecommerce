de6f8c48cbff851d73ae823c118d4033806e18c5
anamariasosa <anamariasosam@gmail.com>
2016-06-05 12:26:07 -0500

    [add] the new session token from instagram-omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def instagram
-    session["devise.instagram_data"] = request.env["omniauth.auth"]
-  
+
     if user = User.from_omniauth(request.env["omniauth.auth"])
       sign_in_and_redirect user
+      session["devise.instagram_data"] = request.env["omniauth.auth"]
     else
       redirect_to new_user_registration_path
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,29 @@
 class UsersController < ApplicationController
 
-before_filter :authenticate_user!
-before_filter :current_user
+  before_filter :authenticate_user!
 
   def dashboard
-    session["devise.instagram_data"]
-    # https://api.instagram.com/v1/users/self/media/recent/?access_token=ACCESS-TOKEN
 
+      if current_user.user_token
+        client = Instagram.client(:access_token => current_user.user_token)
+      else
+        client = Instagram.client(:access_token => session["devise.instagram_data"]["credentials"]["token"])
+        current_user.update( user_token: session["devise.instagram_data"]["credentials"]["token"])
+      end
+
+      @recent_media_items = client.user_recent_media
+      current_user.update(
+        instagram_id:   client.user.id,
+        store_image:    client.user.profile_picture,
+        store_account:  client.user.username
+      )
+      # @options = { count: 20}
+      # @options[:max_id] = params[:max_id] if params[:max_id]
+
+      # @recent_media_items = client.user_recent_media("self", @options)
+
+      # if @recent_media_items.last.nil?
+      #   @recent_media_items = client.user_recent_media
+      # end
   end
-
 end
