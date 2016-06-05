d7ca76830933656adbc4abb817b9cd28d74c608b
anamariasosa <anamariasosam@gmail.com>
2016-06-05 12:26:27 -0500

    [add] after signing method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    users_dashboard_path
+  end
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :username
