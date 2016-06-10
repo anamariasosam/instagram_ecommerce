cdc012588b0b799749c81528b331a1189127fcf4
anamariasosa <anamariasosam@gmail.com>
2016-06-09 23:01:56 -0500

    [improve] dashboard logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,19 @@ class UsersController < ApplicationController
 
   def dashboard
 
-      if current_user.user_token
+      if current_user.user_token?
         client = Instagram.client(:access_token => current_user.user_token)
+        @recent_media_items = client.user_recent_media
+        current_user.update(
+          instagram_id:   client.user.id,
+          store_image:    client.user.profile_picture,
+          store_account:  client.user.username
+        )
       else
-        client = Instagram.client(:access_token => session['super_token'])
-        current_user.update( user_token: session['super_token'])
+        redirect_to edit_user_registration_path
       end
 
-      @recent_media_items = client.user_recent_media
-      current_user.update(
-        instagram_id:   client.user.id,
-        store_image:    client.user.profile_picture,
-        store_account:  client.user.username
-      )
+
       # @options = { count: 20}
       # @options[:max_id] = params[:max_id] if params[:max_id]
 
