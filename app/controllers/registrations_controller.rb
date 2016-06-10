407519eea3d102548f356995d2e3da707c431db0
anamariasosa <anamariasosam@gmail.com>
2016-06-09 22:56:01 -0500

    [add] user_token to the params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def new
-    @user = User.new
-  end
+  protected
+
+    def after_update_path_for(resource)
+      products_path
+    end
 
   private
 
@@ -11,10 +13,10 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
     def sign_up_params
-      params.require(:user).permit(:store_name,:store_account, :email, :phone_number, :address, :delivery_info )
+      params.require(:user).permit(:store_name,:store_account, :email, :phone_number, :address, :delivery_info, :user_token )
     end
 
     def account_update_params
-      params.require(:user).permit(:store_name, :store_account, :email, :phone_number, :address, :delivery_info)
+      params.require(:user).permit(:store_name, :store_account, :email, :phone_number, :address, :delivery_info, :user_token)
     end
 end
