0d9eef4751dfa9a23b48c4561f7fd41cea87f9b4
anamariasosa <anamariasosam@gmail.com>
2016-06-09 22:56:18 -0500

    [remove] spaces

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,16 +1,12 @@
 class StoresController < ApplicationController
 
   def show
-
     @user = User.find(params[:id])
     @products = @user.products.page(params[:page]).per(4)
-
   end
 
   def index
-
     @users = User.all
-
   end
 
 end
