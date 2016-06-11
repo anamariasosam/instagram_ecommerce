7b5af5c8a1fb440272988d9c85a2649279bb3941
anamariasosa <anamariasosam@gmail.com>
2016-06-10 19:42:53 -0500

    [add] more text on copy the code

---
+++ app/assets/javascripts/product_code.js
@@ -0,0 +1,19 @@
+function addLink() {
+    //Get the selected text and append the extra info
+    var selection = window.getSelection(),
+        copytext = "Búscame con este código en Catlog: " + selection,
+        newdiv = document.createElement('div');
+
+    //hide the newly created container
+    newdiv.style.position = 'absolute';
+    newdiv.style.left = '-99999px';
+
+    //insert the container, fill it with the extended text, and define the new selection
+    document.body.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+
+    window.setTimeout(function () {
+        document.body.removeChild(newdiv);
+    }, 100);
+}
