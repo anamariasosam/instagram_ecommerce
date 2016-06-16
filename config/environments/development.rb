97d9cbbe3382b36e2da80e69e34a9a632425063d
anamariasosa <anamariasosam@gmail.com>
2016-06-16 18:41:38 -0500

    remove heroku links

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,14 +37,6 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   config.action_mailer.default_url_options = {:host => "localhost:3000"}
-  ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "25",
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN']
-  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
