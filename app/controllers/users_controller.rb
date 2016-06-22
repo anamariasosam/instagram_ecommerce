900a8331315229b457a5d3ccb57eb61ff5c258e0
anamariasosa <anamariasosam@gmail.com>
2016-06-22 15:58:40 -0500

    [fix] users controller url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :only => [:dashboard]
 
   def show
     @user = User.find(params[:id])
