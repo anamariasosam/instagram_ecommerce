a37ef0bbbe029b48a473a968ecd31f854df62d47
Alex <alexlopezarroyave@gmail.com>
2016-06-25 15:59:31 -0500

    add new active admin resources for product and user

---
+++ app/admin/user.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register User do
+  permit_params(
+    :store_name,
+    :phone_number,
+    :address,
+    :store_image,
+    :delivery_info,
+    :email,
+    # only on a very specific and verified case
+    # you should change this param otherwise is dangerous.
+    # :store_account,
+    :user_token
+  )
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+
+end
