111894003f6c2971ec79ab6a2b3ef0efb5a781d4
anamariasosa <anamariasosam@gmail.com>
2016-06-26 06:36:33 -0500

    add more stores to the dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @users = User.limit(6)
+    @users = User.limit(10)
     @products =  Product.limit(3).order('created_at DESC')
 
     redirect_to "/#{@par.downcase}" if @par = params[:search]
