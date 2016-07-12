087ba6d4369f7e3cf8f902b211548e550e151719
anamariasosa <anamariasosam@gmail.com>
2016-07-12 14:06:05 -0500

    [add] pilot column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629155655) do
+ActiveRecord::Schema.define(version: 20160712173643) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20160629155655) do
     t.string   "provider"
     t.string   "uid"
     t.string   "slug"
+    t.boolean  "pilot"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
