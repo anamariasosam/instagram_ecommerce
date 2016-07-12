30289810558eb3cc00297ca8dec19627ab567d5b
anamariasosa <anamariasosam@gmail.com>
2016-07-12 16:05:05 -0500

    [edit] view and schema

--- db/schema.rb
+++ db/schema.rb
@@ -90,16 +90,16 @@ ActiveRecord::Schema.define(version: 20160716000904) do
     t.string   "store_name"
     t.string   "phone_number"
     t.text     "delivery_info"
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "instagram_id"
-    t.string   "image"
-    t.string   "email",                             default: ""
-    t.string   "encrypted_password",                default: "", null: false
+    t.string   "store_image"
+    t.string   "email",                  default: ""
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -113,7 +113,7 @@ ActiveRecord::Schema.define(version: 20160716000904) do
     t.hstore   "details"
     t.string   "city"
     t.string   "country"
-    t.boolean  "pilot"
+    t.boolean  "pilot",                  default: false
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
