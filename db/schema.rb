a99bb00b4bdd4a2e25d5120a6dc84b4180b2dbae
anamariasosa <anamariasosam@gmail.com>
2016-07-13 11:01:06 -0500

    [add] waiting position to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712173643) do
+ActiveRecord::Schema.define(version: 20160713153551) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20160712173643) do
     t.string   "uid"
     t.string   "slug"
     t.boolean  "pilot",                  default: false
+    t.integer  "waiting_position"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
