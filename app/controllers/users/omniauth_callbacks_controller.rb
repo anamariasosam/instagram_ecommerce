f1e8c1eb40710883aa06026c339a08ac67e9d382
anamariasosa <anamariasosam@gmail.com>
2016-07-14 17:03:43 -0500

    [improve] authentication

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,22 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def instagram
+    if request.env['omniauth.params']['model'] == "store"
+      @store = Store.from_omniauth(request.env["omniauth.auth"])
+      session['super_token'] = request.env["omniauth.auth"]["credentials"]["token"]
 
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    session['super_token'] = request.env["omniauth.auth"]["credentials"]["token"]
-
-    if @user.persisted?
-     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-     set_flash_message(:notice, :success, :kind => "Instagram") if is_navigational_format?
-   else
-     session["devise.instagram_data"] = request.env["omniauth.auth"]
-     redirect_to new_user_registration_url
-   end
+      if @store.persisted?
+       sign_in_and_redirect @store, :event => :authentication #this will throw if @store is not activated
+       set_flash_message(:notice, :success, :kind => "Instagram") if is_navigational_format?
+     else
+       session["devise.instagram_data"] = request.env["omniauth.auth"]
+       redirect_to new_store_registration_url
+     end
+    end
 
   end
 
   def failure
     redirect_to root_path
   end
-  
+
 end
