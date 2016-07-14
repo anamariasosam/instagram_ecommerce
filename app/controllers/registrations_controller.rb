2f5ba7a08fded52e6fc7e2cd54b3405894f73226
anamariasosa <anamariasosam@gmail.com>
2016-07-14 17:03:32 -0500

    [add] new fields

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,6 +12,6 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
     def account_update_params
-      params.require(:user).permit(:store_name, :store_account, :email, :phone_number, :address, :delivery_info, :user_token, :waiting_position)
+      params.require(:user).permit(:store_name, :store_account, :email, :phone_number, :address, :delivery_info, :user_token, :name, :waiting_position)
     end
 end
