0b5457a3c872c296838d63d2826655924ff20097
anamariasosa <anamariasosam@gmail.com>
2016-07-14 17:04:45 -0500

    [comment] user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!, :only => [:dashboard]
 
   def show
-    @user = User.find_by(slug: params[:id])
-    @products = @user.products.page(params[:page]).per(24).order('created_at DESC')
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
+    # @user = User.find_by(slug: params[:id])
+    # @products = @user.products.page(params[:page]).per(24).order('created_at DESC')
+    # respond_to do |format|
+    #   format.html # show.html.erb
+    #   format.json { render json: @user }
+    # end
 
   end
 
