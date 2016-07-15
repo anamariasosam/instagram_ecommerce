35272d4ef8cee8bce72b96d4b26fa18bfca6f744
anamariasosa <anamariasosam@gmail.com>
2016-07-15 10:39:12 -0500

    [run] annotate gem Conflicts: 	app/models/store.rb

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,40 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  store_name             :string
+#  phone_number           :string
+#  address                :string
+#  delivery_info          :text
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  instagram_id           :string
+#  store_image            :string
+#  email                  :string           default("")
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  user_token             :string
+#  store_account          :string
+#  provider               :string
+#  uid                    :string
+#  slug                   :string
+#  type                   :string(15)
+#  details                :hstore
+#  city                   :string
+#
+
 class Store < User
   hstore_accessor :details,
   name: :string,
-  info: :text,
-  slug: :string
+  info: :string
+
+  has_many :products, dependent: :destroy
 end
