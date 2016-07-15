820194a126fbf5ad71ef0fba7e2650086c071cda
anamariasosa <anamariasosam@gmail.com>
2016-07-15 10:34:49 -0500

    [add] column city to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160716000904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
-=======
-ActiveRecord::Schema.define(version: 20160713232351) do
->>>>>>> [edit] database schema
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
@@ -131,17 +126,13 @@ ActiveRecord::Schema.define(version: 20160713232351) do
     t.integer  "waiting_position"
     t.string   "type",                   limit: 15
     t.hstore   "details"
+    t.string   "city"
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["slug"], name: "index_users_on_slug", using: :btree
 
   add_foreign_key "products", "categories"
-<<<<<<< HEAD
   add_foreign_key "products", "users", column: "store_id"
 
-=======
-  add_foreign_key "products", "stores"
-  add_foreign_key "products", "users"
->>>>>>> [edit] database schema
 end
