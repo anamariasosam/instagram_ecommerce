35b5b8da96dd1f3ecae8b1396815365d27d6b1af
anamariasosa <anamariasosam@gmail.com>
2016-07-15 18:17:32 -0500

    [add] new country to user and new schema

--- db/schema.rb
+++ db/schema.rb
@@ -59,11 +59,6 @@ ActiveRecord::Schema.define(version: 20160716000904) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "customers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", force: :cascade do |t|
     t.string   "product_name"
     t.integer  "price"
@@ -74,11 +69,11 @@ ActiveRecord::Schema.define(version: 20160716000904) do
     t.datetime "updated_at",                  null: false
     t.string   "photo_id"
     t.integer  "category_id",     default: 1
-    t.integer  "store_id"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
   add_index "products", ["store_id"], name: "index_products_on_store_id", using: :btree
+
 
   create_table "shortened_urls", force: :cascade do |t|
     t.integer  "owner_id"
@@ -102,9 +97,9 @@ ActiveRecord::Schema.define(version: 20160716000904) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.string   "instagram_id"
-    t.string   "store_image"
-    t.string   "email",                  default: ""
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "image"
+    t.string   "email",                             default: ""
+    t.string   "encrypted_password",                default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -127,12 +122,17 @@ ActiveRecord::Schema.define(version: 20160716000904) do
     t.string   "type",                   limit: 15
     t.hstore   "details"
     t.string   "city"
+    t.string   "country"
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["slug"], name: "index_users_on_slug", using: :btree
 
   add_foreign_key "products", "categories"
+<<<<<<< HEAD
   add_foreign_key "products", "users", column: "store_id"
 
+=======
+  add_foreign_key "products", "users"
+>>>>>>> [add] new country to user and new schema
 end
