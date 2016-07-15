4b4ca65bfb646c3cef0a6bd7fa06fa91c9c26190
anamariasosa <anamariasosam@gmail.com>
2016-07-15 10:38:37 -0500

    [add] show action

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,26 +1,8 @@
 class StoresController < ApplicationController
-  def new
-    @store = Store.new
-  end
+
 
   def show
-    @store = Store.find(params[:id])
+    @store = Store.find_by(slug: params[:id])
   end
 
-  def create
-    @store = Store.new(store_params)
-    if @store.save
-      sign_in @store
-      redirect_to root_path
-    else
-      render 'new'
-    end
-  end
-
-  private
-
-    def store_params
-      params.require(:store).permit(:info, :name, :email, :address,
-                                   :phone_number, :user_token)
-    end
 end
