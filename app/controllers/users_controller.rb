df949f93b30b6cd7e598c57ae40404c9fe92bdcf
anamariasosa <anamariasosam@gmail.com>
2016-07-15 21:02:17 -0500

    [remove] show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 
         current_user.update(
           instagram_id:   client.user.id,
-          image:    client.user.profile_picture,
+          store_image:    client.user.profile_picture,
           store_account:  client.user.username,
           slug:           client.user.username
         )
