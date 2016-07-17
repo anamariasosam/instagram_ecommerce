ea621d309088ac480f1047c0410c3d758f9ead2c
Alex <alexlopezarroyave@gmail.com>
2016-07-16 22:19:16 -0500

    fix tabs in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,34 +11,34 @@ class UsersController < ApplicationController
   end
 
   def dashboard
-      if current_user.user_token?
-        if !session['super_token'].blank?
-          client = Instagram.client(:access_token => session['super_token'])
-          current_user.update(user_token: session['super_token'])
-        else
-          client = Instagram.client(:access_token => current_user.user_token)
-        end
+    if current_user.user_token?
+      if !session['super_token'].blank?
+        client = Instagram.client(:access_token => session['super_token'])
+        current_user.update(user_token: session['super_token'])
+      else
+        client = Instagram.client(:access_token => current_user.user_token)
+      end
 
-        current_user.update(
-          instagram_id:   client.user.id,
-          store_image:    client.user.profile_picture,
-          store_account:  client.user.username,
-          slug:           client.user.username
-        )
+      current_user.update(
+        instagram_id:   client.user.id,
+        store_image:    client.user.profile_picture,
+        store_account:  client.user.username,
+        slug:           client.user.username
+      )
 
-        if current_user.pilot?
-          @options = { count: 40}
-          @options[:max_id] = params[:max_id] if params[:max_id]
-          @media = client.user_recent_media("self", @options)
+      if current_user.pilot?
+        @options = { count: 40}
+        @options[:max_id] = params[:max_id] if params[:max_id]
+        @media = client.user_recent_media("self", @options)
 
-          if @media.last.nil?
-            @media = client.user_recent_media
-          end
-        else
-          redirect_to users_suscribe_path
+        if @media.last.nil?
+          @media = client.user_recent_media
         end
       else
-        redirect_to edit_user_registration_path
+        redirect_to users_suscribe_path
       end
+    else
+      redirect_to edit_user_registration_path
+    end
   end
 end
