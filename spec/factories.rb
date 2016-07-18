103ef76d69c5bafca80275d29791a2cb5206ee37
anamariasosa <anamariasosam@gmail.com>
2016-07-18 10:37:50 -0500

    [edit] factories user_id and delivery_info to store_id and info

--- spec/factories.rb
+++ spec/factories.rb
@@ -13,14 +13,14 @@ FactoryGirl.define do
     instagram_image "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/1173135_1016593388379223_512156499_n.jpg"
     description "Nuevo mani"
     photo_id "2"
-    user_id "1"
+    store_id "1"
     category_id "1"
   end
 
   factory :user do
     store_name '@anasosam23'
     phone_number '1234567890'
-    delivery_info 'fasdfasd'
+    info 'fasdfasd'
     instagram_id 'fsad86fs9a87'
     image 'fasdf'
     email 'ana@ama.com'
