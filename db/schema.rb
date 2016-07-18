3486f76e8de4f9870962afa89920cf7b67b87e0a
anamariasosa <anamariasosam@gmail.com>
2016-07-18 10:17:55 -0500

    [edit] schema after run the migration

--- db/schema.rb
+++ db/schema.rb
@@ -111,10 +111,10 @@ ActiveRecord::Schema.define(version: 20160716000904) do
     t.string   "slug"
     t.string   "type",                   limit: 15
     t.hstore   "details"
-    t.boolean  "pilot",                             default: false
-    t.integer  "waiting_position"
     t.string   "city"
     t.string   "country"
+    t.boolean  "pilot",                             default: false
+    t.integer  "waiting_position"
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
