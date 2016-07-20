0ce4f576e5398248de3d6ba976a39c9edce37795
anamariasosa <anamariasosam@gmail.com>
2016-07-20 15:30:46 -0500

    [add] customer omniauth login
    [add] Customer controller action liked
    
    [add] Customer name and address
    
    [add] Customer dashboard
    
    [add] login modal to application layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
-  before_filter :authenticate_user!, :only => [:dashboard, :suscribe]
+  before_filter :authenticate_user!, only: [:dashboard, :suscribe]
+  before_filter :require_store, only: [:dashboard, :suscribe]
 
   def oauth_failure
   end
@@ -30,17 +31,43 @@ class UsersController < ApplicationController
         @options = { count: 40}
         @options[:max_id] = params[:max_id] if params[:max_id]
         @media = client.user_recent_media("self", @options)
-
         if @media.last.nil?
           @media = client.user_recent_media
         end
       else
         redirect_to users_suscribe_path
       end
-
     else
       redirect_to edit_user_registration_path
     end
   end
+
+  def liked
+    if current_user.user_token?
+      if !session['super_token'].blank?
+        client = Instagram.client(:access_token => session['super_token'])
+        current_user.update(user_token: session['super_token'])
+      else
+        client = Instagram.client(:access_token => current_user.user_token)
+      end
+
+      current_user.update(
+        instagram_id:   client.user.id,
+        image:    client.user.profile_picture,
+        store_account:  client.user.username,
+        slug:           client.user.username
+      )
+    else
+      redirect_to edit_user_registration_path
+    end
+  end
+
+  private
+    def require_store
+      if current_user.type == "Customer"
+        flash[:error] = "No eres tienda"
+        redirect_to root_url
+      end
+    end
 
 end
