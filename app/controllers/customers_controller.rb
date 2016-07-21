a5aebcedfe2b6a20d21968660fc9bcc8e304fd83
anamariasosa <anamariasosam@gmail.com>
2016-07-21 12:57:05 -0500

    [create] methods to get instagram photos

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,23 +1,51 @@
 class CustomersController < ApplicationController
   before_filter :authenticate_user!
+
+  def fillArray(data,array,item)
+    data.each do |media|
+     array.push(media[item])
+    end
+  end
+
+  def getMedia()
+    instagram_photos_id = []
+    products_id = []
+    products_all = Product.all
+
+    response = HTTParty.get("https://api.instagram.com/v1/users/self/media/liked?access_token=" + current_user.user_token)
+    media_liked = response['data']
+
+    fillArray(media_liked, instagram_photos_id, 'id' )
+    fillArray(products_all, products_id, 'photo_id' )
+
+    return instagram_photos_id & products_id
+  end
   
+  def getProducts()
+    @products = []
+    media_id = getMedia()
+
+    media_id.each do |media_item|
+      @products.push(Product.find_by(photo_id: media_item))
+    end
+
+    @product = @products.shift()
+
+    if @products.empty?
+      redirect_to product_path(@product)
+    end
+
+  end
+
   def dashboard
     if current_user.user_token?
       if !session['super_token'].blank?
-        client = Instagram.client(:access_token => session['super_token'])
         current_user.update(user_token: session['super_token'])
-      else
-        client = Instagram.client(:access_token => current_user.user_token)
       end
-
-      current_user.update(
-        instagram_id:   client.user.id,
-        image:    client.user.profile_picture,
-        store_account:  client.user.username,
-        slug:           client.user.username
-      )
+      getProducts()
     else
       redirect_to edit_user_registration_path
     end
   end
+
 end
