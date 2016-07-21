2fcc144409dc90e97fec2426d669dbff4c20abf8
anamariasosa <anamariasosam@gmail.com>
2016-07-20 22:21:53 -0500

    [create] order with product,store and customer associations

---
+++ db/migrate/20160721014421_create_orders.rb
@@ -0,0 +1,9 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.string :status
+      t.references :product, index: true, foreign_key: true
+      t.timestamps null: false
+    end
+  end
+end
