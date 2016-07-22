be370bddbf84b365582fcaf141669c8dd6b53f0a
anamariasosa <anamariasosam@gmail.com>
2016-07-21 19:33:32 -0500

    [add] products table links helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 module ApplicationHelper
-
+  def sortable(title, column)
+    direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
+    link_to title, sort: column, direction: direction
+  end
 end
