e450180a19ab7c8199f6442f3dda9847a1958317
Alex <alexlopezarroyave@gmail.com>
2016-07-23 23:57:50 -0500

    destroy orders

--- app/models/order.rb
+++
@@ -1,5 +0,0 @@
-class Order < ActiveRecord::Base
-  belongs_to :product
-  belongs_to :customer
-  belongs_to :store
-end
--- app/views/orders/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @order, :id, :status, :product_id, :customer_id, :store_id, :created_at, :updated_at
