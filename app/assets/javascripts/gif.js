3efbb0def989e5553c26db92ba716160b34b45a5
anamariasosa <anamariasosam@gmail.com>
2016-07-28 11:41:57 -0500

    Remove all actions, styles and views of search a product with code, remove the tutorials gifs from assets because where are using vimeo videos. Add the searchModal-btn style to the new login-btn in the menu

--- app/assets/javascripts/gif.js
+++ app/assets/javascripts/gif.js
@@ -2,9 +2,7 @@ function showGif() {
   var url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=happy"
   $.get( url, function( data ) {
     $('#js_celebrateGif').attr('src', data.data.image_url);
-    console.log(data);
   });
-
 }
 
 $(document).on('ready', function() {
