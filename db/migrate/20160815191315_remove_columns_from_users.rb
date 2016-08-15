b260c826602acdd08de89db45685dbe5e80a0c35
anamariasosa <anamariasosam@gmail.com>
2016-08-15 15:21:58 -0500

    remove comment in the migration file

--- db/migrate/20160815191315_remove_columns_from_users.rb
+++ db/migrate/20160815191315_remove_columns_from_users.rb
@@ -1,5 +1,4 @@
 class RemoveColumnsFromUsers < ActiveRecord::Migration
-  # Remove the column store_name and delivery_info
   def change
     remove_column :users, :store_name
     remove_column :users, :delivery_info
