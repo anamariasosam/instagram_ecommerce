59b3491d694b2070ccd6723c366fa7bcb4db3e52
anamariasosa <anamariasosam@gmail.com>
2016-08-15 15:15:26 -0500

    reverse last commit

--- db/migrate/20160815191315_remove_columns_from_users.rb
+++ db/migrate/20160815191315_remove_columns_from_users.rb
@@ -1,4 +1,5 @@
 class RemoveColumnsFromUsers < ActiveRecord::Migration
+  # Remove the column store_name and delivery_info
   def change
     remove_column :users, :store_name
     remove_column :users, :delivery_info
