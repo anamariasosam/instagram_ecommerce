1405a900db4d833e45350671df5388b1bffc3bca
anamariasosa <anamariasosam@gmail.com>
2016-08-15 18:28:12 -0500

    Add email and name to contact form is the user is authenticated

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,29 @@
 class ContactsController < ApplicationController
   def new
     @contact = Contact.new
+    populate_data
   end
 
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:error] = nil
+      flash.now[:error] = t('mail_form.success')
     else
-      flash.now[:error] = 'No se puede enviar el mensaje.'
+      flash.now[:error] = t('mail_form.error')
       render :new
     end
   end
+
+  private
+    def populate_data
+      if user_signed_in?
+        @email = current_user.email
+        if current_user.type == "Store"
+          @name = current_user.name
+        else
+          @name = current_user.full_name
+        end
+      end
+    end
 end
