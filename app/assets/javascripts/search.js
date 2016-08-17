a42ea136624db526eda857f5ea59755db223d8cc
anamariasosa <anamariasosam@gmail.com>
2016-08-17 16:20:47 -0500

    Add autofocus to search modal input and add regex validation of the code

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -18,18 +18,25 @@ function getInput() {
   }
 }
 
-// Validates the input value is not empty
+// Validates the input value is empty or invalid
 function validate_search_req( event ) {
   var $input = getInput();
-  if ( !$input.val() ) {
+  var patt = new RegExp(/^[a-zA-Z0-9]+$/);
+
+  if ( !patt.test($input.val()) && $input.val().length != 5){
     event.preventDefault();
     $input
-      .attr("placeholder", "Recuerda escribir el código de producto");
+      .val('')
+      .attr("placeholder", "Verifica el código");
   }
 }
 
 $(document).on('ready page:change page:load', function() {
-  $('#searchModal').on('show.bs.modal', center_modal);
+  $('#searchModal').on('shown.bs.modal', function() {
+    center_modal()
+    $(this).find('[autofocus]').focus();
+  });
+
   $(window).on('resize', function() {
       $('.modal:visible').each(center_modal);
   });
