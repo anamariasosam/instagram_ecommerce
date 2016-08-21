08d6967075ba6bb6c68a9203a9dcb8c90c78ea78
Alex <alexlopezarroyave@gmail.com>
2016-08-21 18:19:50 -0500

    add recent "bolsos" to home page. this closes #159

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   def index
     @stores = Store.limit(10)
     @products =  Product.limit(4).order(created_at: :desc).includes(:store)
+
+    @category = Category.find_by_name('Bolsos')
+    @bags = @category.products.limit(4).includes(:store) if @category
+
     redirect_to "/#{@par.downcase}" if @par = params[:search]
   end
 end
