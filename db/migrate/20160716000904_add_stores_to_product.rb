95ffea74c6b72456a101a30df0dddb72c8438564
anamariasosa <anamariasosam@gmail.com>
2016-09-02 19:03:01 -0500

    Add precision to price and remove a Product migration in order to fix paranoia test

--- db/migrate/20160716000904_add_stores_to_product.rb
+++ db/migrate/20160716000904_add_stores_to_product.rb
@@ -2,7 +2,7 @@ class AddStoresToProduct < ActiveRecord::Migration
   def change
     add_column :products, :store_id, :integer
     add_foreign_key :products, :users, column: :store_id, primary_key: :id
-    Product.update_all("store_id = user_id")
+    # Product.update_all("store_id = user_id")
     remove_column :products, :user_id, :integer
   end
 end
