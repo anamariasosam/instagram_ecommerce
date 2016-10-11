51315d50d387c9b48d4a3340e5cfde65a6f04ff1
anamariasosa <anamariasosam@gmail.com>
2016-10-11 18:05:38 -0500

    Remove email me when a store is created

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -27,9 +27,4 @@ class UserNotifier < ApplicationMailer
     mail( :to => @customer.email,
     :subject => "👌 El producto que compraste en Catlog está #{order.status.downcase}" )
   end
-
-  def new_store_created
-    mail( :to => "anamaria@catlog.co",
-    :subject => "👯 Nuevo cliente en catlog.co" )
-  end
 end
